52dda72025097e79c28cd6e70902f6dcea4b88c0
Steve Rich <srichy@mac.com>
2021-12-05 17:47:33 -0500

    basic line splitting and tests

--- Sources/LinesSplits/LinesSplits.swift
+++ Sources/LinesSplits/LinesSplits.swift
@@ -1,6 +1,104 @@
-public struct LinesSplits {
-    public private(set) var text = "Hello, World!"
+//
+//  LinesSplits.swift
+//  D05
+//
+//  Created by Steven Rich on 12/4/21.
+//
 
-    public init() {
+
+import Foundation
+
+public struct Lines: Sequence, IteratorProtocol {
+    let fromFile: String?
+    let fileHandle: FileHandle?
+    let chunkSize: Int
+    var dataCache: Data?
+
+    public typealias Element = String
+
+    @discardableResult
+    mutating func refill() throws -> Bool {
+        if let fh = self.fileHandle {
+            let newData = try fh.read(upToCount: self.chunkSize)
+            if var dc = self.dataCache {
+                if let nd = newData {
+                    dc.append(nd)
+                }
+            } else {
+                self.dataCache = newData
+            }
+
+            if newData != nil {
+                return true
+            }
+        }
+        return false
+    }
+
+    public mutating func next() -> Self.Element? {
+        if self.dataCache == nil {
+            do {
+                try self.refill()
+            } catch {
+                return nil
+            }
+        }
+        if self.dataCache == nil {
+            return nil
+        }
+
+        repeat {
+            let parts = self.dataCache!.split(separator: 10,
+                                              maxSplits: 1,
+                                              omittingEmptySubsequences: false)
+            self.dataCache = nil
+            if parts.count == 2 {
+                self.dataCache = parts[1]
+                return String(data: parts[0], encoding: .ascii)
+            }
+            // If we reach here, then we split and got only a single Data element.
+            // That means no NL character.  We may be at the very end of our buffer
+            // and just need to refill it.  Try that.
+            do {
+                if try self.refill() {
+                    // Ok.  We got more data.  Let's append that to
+                    // our single chunk and try again.
+                    var newCache = parts[0]
+                    newCache.append(self.dataCache!)
+                    self.dataCache = newCache
+                } else {
+                    // No more data.  Must be at EOL.  Return what we have.
+                    // BUT WAIT!  There's more!  Inexplicably, a Data.split()
+                    // on a zero-length Data instance returns... wait for it...
+                    // A one-length array of [Data] with a single zero-length
+                    // Data instance.  Why?  IDK.  IMO, it should return a
+                    // zero-length [Data] Array.  Oh well.  Life goes on.
+                    // However, one must distinguish between and original
+                    // zero-length Data and a Data with a single sep character...
+                    if parts[0].count == 0 {
+                        return nil
+                    }
+                    return String(data: parts[0], encoding: .ascii)
+                }
+            } catch {
+                // Something failed in read, but we do have data.  Return that.
+                // The next iteration may fail-fail
+                return String(data: parts[0], encoding: .ascii)
+            }
+        } while true
+    }
+
+    public init(fromFile: String, chunkSize:Int = 65536) throws {
+        self.fromFile = fromFile
+        self.fileHandle = FileHandle.init(forReadingAtPath: fromFile)!
+        self.chunkSize = chunkSize
+        self.dataCache = nil
+    }
+
+    public init(fromData: Data) {
+        self.fromFile = nil
+        self.fileHandle = nil
+        self.chunkSize = 0
+        self.dataCache = fromData
     }
 }
--- Tests/LinesSplitsTests/LinesSplitsTests.swift
+++ Tests/LinesSplitsTests/LinesSplitsTests.swift
@@ -2,10 +2,40 @@ import XCTest
 @testable import LinesSplits
 
 final class LinesSplitsTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(LinesSplits().text, "Hello, World!")
+
+    func testDataLinesEmpty() throws {
+        var ls = Lines(fromData: Data())
+
+        XCTAssertEqual(ls.next(), nil)
+    }
+
+    func testDataLinesStub() throws {
+        var ls = Lines(fromData: "\n".data(using: .utf8)!)
+
+        XCTAssertEqual(ls.next(), "")
+        XCTAssertEqual(ls.next(), nil)
+    }
+
+    func testDataLinesOne() throws {
+        var ls = Lines(fromData: "single line".data(using: .utf8)!)
+
+        XCTAssertEqual(ls.next(), "single line")
+        XCTAssertEqual(ls.next(), nil)
+    }
+
+    func testDataLinesTwo() throws {
+        var ls = Lines(fromData: "line 1\nline 2".data(using: .utf8)!)
+
+        XCTAssertEqual(ls.next(), "line 1")
+        XCTAssertEqual(ls.next(), "line 2")
+        XCTAssertEqual(ls.next(), nil)
+    }
+
+    func testDataLinesTwoPlus() throws {
+        var ls = Lines(fromData: "line 1\nline 2\n".data(using: .utf8)!)
+
+        XCTAssertEqual(ls.next(), "line 1")
+        XCTAssertEqual(ls.next(), "line 2")
+        XCTAssertEqual(ls.next(), nil)
     }
 }
